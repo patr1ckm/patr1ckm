7f135b11adc7272c1db7072a80ab8474c228b88e
patr1ckm <patrick.mil10@gmail.com>
2016-01-19 16:43:51 -0500

    tests for is.special

---
+++ tests/testthat/test_special.R
@@ -0,0 +1,12 @@
+expect_true(is.special(NA))
+expect_true(is.special(NaN))
+expect_true(is.special(Inf))
+expect_true(is.special(-Inf))
+
+#expect_true(is.special(NULL))
+expect_true(!is.special(1))
+expect_true(!is.special(TRUE))
+expect_true(!is.special(as.factor(1)))
+expect_true(!is.special("a"))
+expect_true(!is.special(FALSE))
+expect_true(!is.special(1.0))
