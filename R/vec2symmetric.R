c395247481d79795f5a90b253e1ef0a87973e85c
patr1ckm <patrick.mil10@gmail.com>
2016-02-09 17:24:30 -0500

    added vec2sym

---
+++ R/vec2symmetric.R
@@ -0,0 +1,27 @@
+#' convert vector to symmetric matrix
+#'
+#' Given a vector of upper or lower triangular elements of a matrix, returns the
+#' corresponding symmetric matrix. The elements of the vector can be arranged by
+#' column (the default) or by row.
+#'
+#' @param x vector containing upper or lower triangular elements of a symmetric matrix
+#' @param diagonal value or vector of elements to place on the diagonal
+#' @param lower x is from the lower triangle (default = \code{TRUE})
+#' @param byrow the elements in x are ordered row-wise (default = \code{FALSE})
+#' @export
+vec2sym <- function(x,diagonal=NULL,lower=TRUE,byrow=FALSE){
+  p <- (sqrt(1 + 8 * length(x)) - 1)/2 # solution to quadratic formula
+  S <- diag(p)
+  if((!lower & byrow) | (lower & !byrow)){
+    S[lower.tri(S)] <- x
+    S[which(lower.tri(t(S)),arr.ind=T)[,c(2,1)]] <- x
+  }
+  if((lower & byrow) | (!lower & !byrow)) {
+    S[upper.tri(S)] <- x
+    S[which(upper.tri(t(S)),arr.ind=T)[,c(2,1)]] <- x
+  }
+  if(!is.null(diagonal)){
+    diag(S) <- diagonal
+  }
+  return(S)
+}
---
+++ tests/testthat/test_vec2symm.R
@@ -0,0 +1,13 @@
+
+
+x <- c(1,1,1,2,2,3)
+check <- matrix(c(1,1,1,1,1,1,2,2,1,2,1,3,1,2,3,1),4,4)
+expect_equal(vec2sym(x,diag=1,lower=T,byrow=F), check) # lower tri by col
+expect_equal(vec2sym(x,diag=1,lower=F,byrow=T), check) # upper tri by row
+
+x <- c(1,2,2,3,3,3)
+check <- matrix(c(1,1,2,3,1,1,2,3,2,2,1,3,3,3,3,1),4,4)
+expect_equal(vec2sym(x,diag=1,lower=T,byrow=T),check) # lower tri by row
+expect_equal(vec2sym(x,diag=1,lower=F,byrow=F),check) # upper tri by col
+
+expect_equal(diag(vec2sym(x,diag=0,lower=F,byrow=F)),rep(0,4)) # check diag argument
