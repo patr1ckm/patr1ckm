75c6059560d67ce5232ac1771412597858170d19
patr1ckm <patrick.mil10@gmail.com>
2016-03-16 16:00:48 -0400

    fixed gapply bug

--- tests/testthat/test_gapply.R
+++ tests/testthat/test_gapply.R
@@ -9,7 +9,7 @@ expect_equivalent(unique(r),3)
 
 
 out <- gapply(do.one,reps=2, a=1:2,b=2)
-expect_equal(colnames(out), c("a", "b", "param.id", "V1"))
+expect_equal(colnames(out), c("param.id","a", "b", "V1"))
 expect_equal(nrow(out), nrow(expand.grid(a=1:2,b=2))*2)
 
 f <- do.rep
