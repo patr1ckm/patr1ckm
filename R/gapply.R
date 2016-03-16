3bd18c16e6d95c81bc545b94168ad1c9f50fd179
patr1ckm <patrick.mil10@gmail.com>
2016-03-16 16:08:41 -0400

    added reps column

--- R/gapply.R
+++ R/gapply.R
@@ -20,7 +20,9 @@ gapply <- function(f, reps=1, mc.cores=1,...){
   param.ls <- split(param.grid, 1:nrow(param.grid))
   names(param.ls) <- NULL
   res <- mclapply(param.ls, do.rep, f=f, reps=reps,mc.cores=mc.cores)
-  long <- as.data.frame(cbind(do.call(rbind,res),param.id=rep(1:nrow(param.grid),each=reps)))
+  long <- as.data.frame(cbind(param.id=rep(1:nrow(param.grid),each=reps),
+                              reps=rep(1:nrow(param.grid), times=reps),
+                              do.call(rbind,res)))
   param.grid <- data.frame(param.grid, param.id=1:nrow(param.grid))
   long.param <- merge(param.grid,long)
   return(long.param)
--- tests/testthat/test_gapply.R
+++ tests/testthat/test_gapply.R
@@ -7,24 +7,25 @@ r <- do.rep(do.one,reps=8,list(a=1,b=2))
 expect_equal(nrow(r),8)
 expect_equivalent(unique(r),3)
 
-
 out <- gapply(do.one,reps=2, a=1:2,b=2)
+grid <- expand.grid(a=1:2,b=2)
 expect_equal(colnames(out), c("param.id","a", "b", "V1"))
-expect_equal(nrow(out), nrow(expand.grid(a=1:2,b=2))*2)
-
-f <- do.rep
-reps <- 1
-param.grid <- expand.grid(a=1:2,b=2)
-mc.cores <- 1
+expect_equal(nrow(out), nrow(grid)*2)
+expect_equivalent(out$V1,c(3,3,4,4))
+expect_equivalent(unique(out[,c("a","b")]), grid)
 
 do.one <- function(a=1,b=2){c(a+b,a-b)}
 r <- do.rep(do.one,reps=2,list(a=1,b=2))
 expect_equal(colnames(r), c("V1","V2"))
 out <- gapply(do.one,reps=2, a=1:2,b=2)
-expect_equal(colnames(out),c("a","b","param.id","V1", "V2"))
+expect_equal(colnames(out),c("param.id","a","b","V1", "V2"))
 
 do.one <- function(a=1,b=2){data.frame(sum=a+b,sub=a-b)}
 (out <- gapply(do.one,reps=2, a=1:2,b=2))
-expect_equal(colnames(out),c("a","b","param.id","sum","sub"))
+expect_equal(colnames(out),c("param.id","a","b","sum","sub"))
 
 ## add reps
+f <- do.one
+reps <- 2
+param.grid <- expand.grid(a=1:2,b=2)
+mc.cores <- 1
