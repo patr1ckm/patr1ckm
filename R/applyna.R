1e2bc0d969aaaed39006d3483ab2b7e37dbbcfc8
patr1ckm <patrick.mil10@gmail.com>
2016-03-25 14:33:10 -0400

    added FUN to applyna

--- R/applyna.R
+++ R/applyna.R
@@ -1,7 +1,15 @@
 
-#' Apply is.na to elements in margin of array or matrix
+#' Apply is.na to vector in margin of array or matrix
 #'
 #' @param x array, matrix, or data.frame
 #' @param mar margin: 1=rows, 2=columns, etc.
+#' @param FUN function to be applied to \code{is.na(vector)}, 
+#' where \code{vector} is the vector of \code{x} implied by \code{mar}.
+#'  Default is \code{I}, returning a logical vector.
 #' @export
-applyna <- function(x,mar=2){ apply(x,mar,function(v){is.na(v)})}
+#' @examples
+#' x <- matrix(5,5,5)
+#' x[2,3] <- NA
+#' applyna(x,2)
+#' applyna(x,2,mean)
+applyna <- function(x,mar=2,FUN=I){ apply(x,mar,function(v){FUN(is.na(v))})}
