febe3495ce054f730d8de677d039604d9cb97cad
patr1ckm <patrick.mil10@gmail.com>
2016-04-09 15:28:25 -0400

    adding reup

---
+++ R/reup.R
@@ -0,0 +1,26 @@
+#' Quickly reinstall the already loaded package from github
+#' 
+#' @export
+reup <- function(x){
+  remove.packages("patr1ckm")
+  devtools::install_github("patr1ckm/patr1ckm")
+  library(patr1ckm)
+}
+
+#' Push to github
+#' 
+#' @export
+push <- function(m, f=NULL){
+  if(!is.null(f)){ 
+    cmd <- paste0("git add R/", f)
+    system(cmd)
+  }
+  cmd <- "git add -u"
+  system(cmd)
+  cmd <- paste0("git commit -m '", m, "'")
+  system(cmd)
+  cmd <- paste0("git push origin master")
+  system(cmd)
+}
+
+## testing
